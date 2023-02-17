ccb73d7b7fe468ae50c4f6b35a279c2a1024eb07
Lan Tzu Hua <ray.lan@tpisoftware>
2023-02-17 18:42:04 +0800

    可成功進行 rabbitmq round robin 機制實驗

---
+++ consumer.ts
@@ -0,0 +1,51 @@
+import amqp from 'amqp-connection-manager';
+
+// common setting
+const rabbitUrl = 'amqp://localhost:5672';
+const exchangeName = 'eexxchange';
+const queueName = 'qquueue';
+
+// consumer setting
+const consumerName = process.argv[2] || 'consumer';
+
+// create connection
+const connection = await amqp.connect([rabbitUrl]);
+connection.on('connect', () => {
+  console.log('Successfully connect to rabbitmq');
+  console.log(`My name is: ${consumerName}`);
+});
+connection.on('connectFailed', (error) => {
+  console.error('Can not connect to rabbitmq');
+  console.error(error);
+  process.exit(1);
+});
+
+// create channel
+const channel = connection.createChannel();
+await channel.addSetup(() => {
+  return Promise.all([
+    // assert exchange
+    channel.assertExchange(exchangeName, 'direct'),
+    // assert queue
+    channel.assertQueue(queueName, { durable: true }),
+    // bind exchange and queue
+    channel.bindQueue(queueName, exchangeName, queueName),
+  ]);
+});
+
+channel.consume(
+  queueName,
+  (msg) => {
+    const parsedMsg = JSON.parse(msg.content.toString()) as {
+      content: string;
+      processSeconds: number;
+    };
+    console.log(`${consumerName} receive: ${parsedMsg.content}`);
+    console.log(`Waiting Seconds: ${parsedMsg.processSeconds}`);
+    setTimeout(() => {
+      console.log(`${parsedMsg.content} is finished.`);
+      channel.ack(msg);
+    }, parsedMsg.processSeconds * 1000);
+  },
+  { prefetch: 1, noAck: false },
+);
---
+++ producer.ts
@@ -0,0 +1,47 @@
+import amqp from 'amqp-connection-manager';
+
+// common setting
+const rabbitUrl = 'amqp://localhost:5672';
+const exchangeName = 'eexxchange';
+const queueName = 'qquueue';
+
+// producer setting
+/** 要發送的內容 */
+const msgContent = process.argv[2];
+/** 這個內容要等幾秒後，consumer 才能 ack */
+const msgProcessSeconds = Number(process.argv[3]);
+const msg = {
+  content: msgContent,
+  processSeconds: msgProcessSeconds,
+};
+
+// create connection
+const connection = await amqp.connect([rabbitUrl]);
+connection.on('connect', () => {
+  console.log('Successfully connect to rabbitmq');
+});
+connection.on('connectFailed', (error) => {
+  console.error('Can not connect to rabbitmq');
+  console.error(error);
+  process.exit(1);
+});
+
+// create channel
+const channel = connection.createChannel();
+await channel.addSetup(() => {
+  return Promise.all([
+    // assert exchange
+    channel.assertExchange(exchangeName, 'direct'),
+    // assert queue
+    channel.assertQueue(queueName, { durable: true }),
+    // bind exchange and queue
+    channel.bindQueue(queueName, exchangeName, queueName),
+  ]);
+});
+
+// publish message to exchange
+await channel.publish(exchangeName, queueName, JSON.stringify(msg), {
+  // timeout: 10000,
+});
+
+connection.close();
