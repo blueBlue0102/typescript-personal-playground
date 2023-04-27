eecf8390e175a8270945709d24f20fd6e80047a1
Blue <ray1997860102@gmail.com>
2023-04-27 22:49:50 +0800

    建立 4 個情境的檔案

---
+++ test case/1-單一doc的原子性.ts
@@ -0,0 +1,4 @@
+import mongoose from '../mongo-client.js';
+
+// 目標：驗證 https://www.mongodb.com/docs/manual/core/write-operations-atomicity/#atomicity 是否為真？
+// 測試方法：
---
+++ test case/2-racing情境.ts
@@ -0,0 +1,4 @@
+import mongoose from '../mongo-client.js';
+
+// 目標：
+// 測試方法：
